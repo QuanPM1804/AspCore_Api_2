b482998ddc2c5e2541b866b27a88b96bb99e61d7
QuanPM <95131155+QuanPM1804@users.noreply.github.com>
2024-05-09 15:19:24 +0700

    Update Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,13 +1,20 @@
-﻿namespace AspCore_Api_2.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace AspCore_Api_2.Models
 {
     public class Person
     {
         public Guid Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string FullName => LastName + ' ' + FirstName;
+        [Required]
         public DateTime DateOfBirth { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public string BirthPlace { get; set; }
     }
 }
